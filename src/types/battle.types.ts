cd3256606a451438474340ea16e9b9ae119264b1
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-18 22:24:56 +0300

    feat(types): add battle-related types and interfaces

---
+++ src/types/battle.types.ts
@@ -0,0 +1,58 @@
+import type { Pokemon } from './pokemon.types';
+
+export interface BattleResult {
+  id: string;
+  winnerId: string;
+  loserId: string;
+  winnerName: string;
+  loserName: string;
+  xpGained: number;
+  battleLog: string[];
+  date: Date;
+}
+
+export interface BattleSimulation {
+  attacker: Pokemon;
+  defender: Pokemon;
+  turns: BattleTurn[];
+  winner: Pokemon;
+  loser: Pokemon;
+}
+
+export interface BattleTurn {
+  turnNumber: number;
+  attackerId: string;
+  defenderId: string;
+  damage: number;
+  defenderHpRemaining: number;
+  message: string;
+}
+
+export interface RandomOpponent {
+  id: number;
+  name: string;
+  type: PokemonType;
+  hp: number;
+  attack: number;
+  defense: number;
+  speed: number;
+  imageUrl: string;
+  level: number;
+}
+
+export interface BattleHistory {
+  battles: BattleResult[];
+  totalBattles: number;
+  wins: number;
+  losses: number;
+}
+
+export interface BattleStats {
+  pokemonId: string;
+  pokemonName: string;
+  totalBattles: number;
+  wins: number;
+  losses: number;
+  winRate: number;
+  totalXpGained: number;
+}
